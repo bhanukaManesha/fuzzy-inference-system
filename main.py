bc9ac75d90935699862be0e0fd7fa1fd0b37c4b2
bhanukaManesha <bhanukamgamage@gmail.com>
2020-10-12 04:01:10 +0800

    Rename to momentum 🎨

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ import matplotlib.pyplot as plt
 
 # Generate universe variables
 #   * RSI and MACD is has a range [0, 1]
-#   * STOCK ACTION also has a range one [0,1]
+#   * MOMENTUM also has a range one [0,1]
 x_rsi = np.arange(0, 11, 1)
 x_macd = np.arange(0, 11, 1)
-x_stockaction = np.arange(0, 11, 1)
-
+x_momentum = np.arange(0, 11, 1)
 
 # Generate fuzzy membership functions
-rsi_low = fuzz.trapmf(x_rsi, [0, 0, 1, 4])
-rsi_middle = fuzz.trimf(x_rsi, [2, 5, 8])
-rsi_high = fuzz.trapmf(x_rsi, [6, 9, 10, 10])
+rsi_low = fuzz.trapmf(x_rsi, [0, 0, 1, 5])
+rsi_middle = fuzz.trimf(x_rsi, [3, 5, 7])
+rsi_high = fuzz.trapmf(x_rsi, [5, 9, 10, 10])
 
-macd_high = fuzz.trapmf(x_macd, [2, 7, 10, 10])
-macd_low = fuzz.trapmf(x_macd, [0, 0, 3, 8])
+macd_high = fuzz.trapmf(x_macd, [4, 6, 10, 10])
+macd_low = fuzz.trapmf(x_macd, [0, 0, 4, 6])
 
-stockaction_buy = fuzz.trimf(x_stockaction, [0, 0, 4])
-stockaction_hold = fuzz.trapmf(x_stockaction, [0, 3, 7, 10])
-stockaction_sell = fuzz.trimf(x_stockaction, [6, 10, 10])
+momentum_bearish = fuzz.trimf(x_momentum, [0, 0, 4])
+momentum_neutral = fuzz.trapmf(x_momentum, [2, 4, 6, 8])
+momentum_bullish = fuzz.trimf(x_momentum, [6, 10, 10])
 
 # Visualize these universes and membership functions
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
@@ -36,9 +35,9 @@ ax1.plot(x_macd, macd_high, 'g', linewidth=1.5, label='High')
 ax1.set_title('MACD')
 ax1.legend()
 
-ax2.plot(x_stockaction, stockaction_buy, 'b', linewidth=1.5, label='Buy')
-ax2.plot(x_stockaction, stockaction_hold, 'g', linewidth=1.5, label='Hold')
-ax2.plot(x_stockaction, stockaction_sell, 'r', linewidth=1.5, label='Sell')
+ax2.plot(x_momentum, momentum_bearish, 'b', linewidth=1.5, label='Bearish')
+ax2.plot(x_momentum, momentum_neutral, 'g', linewidth=1.5, label='Neutral')
+ax2.plot(x_momentum, momentum_bullish, 'r', linewidth=1.5, label='Bullish')
 ax2.set_title('Stock Action')
 ax2.legend()
 
@@ -51,7 +50,7 @@ for ax in (ax0, ax1, ax2):
 
 plt.tight_layout()
 
-RSI = 5
+RSI = 7
 MACD = 5
 
 
@@ -67,22 +66,22 @@ macd_level_hi = fuzz.interp_membership(x_macd, macd_high, MACD)
 active_rule2 = np.fmin(macd_level_lo, np.fmax(rsi_level_lo,rsi_level_md))
 active_rule3 = np.fmin(macd_level_hi, np.fmax(rsi_level_lo,rsi_level_md))
 
-sell_activation = np.fmin(rsi_level_hi, stockaction_sell)
-hold_activation = np.fmin(active_rule2, stockaction_hold)
-buy_activation = np.fmin(active_rule3, stockaction_buy)
+bullish_activation = np.fmin(rsi_level_hi, momentum_bullish)
+neutral_activation = np.fmin(active_rule2, momentum_neutral)
+bearish_activation = np.fmin(active_rule3, momentum_bearish)
 
-stockaction0 = np.zeros_like(x_stockaction)
+momentum0 = np.zeros_like(x_momentum)
 
 
 # Visualize this
 fig, ax0 = plt.subplots(figsize=(8, 3))
 
-ax0.fill_between(x_stockaction, stockaction0, sell_activation, facecolor='b', alpha=0.7)
-ax0.plot(x_stockaction, stockaction_sell, 'b', linewidth=0.5, linestyle='--', )
-ax0.fill_between(x_stockaction, stockaction0, hold_activation, facecolor='g', alpha=0.7)
-ax0.plot(x_stockaction, stockaction_hold, 'g', linewidth=0.5, linestyle='--')
-ax0.fill_between(x_stockaction, stockaction0, buy_activation, facecolor='r', alpha=0.7)
-ax0.plot(x_stockaction, stockaction_buy, 'r', linewidth=0.5, linestyle='--')
+ax0.fill_between(x_momentum, momentum0, bullish_activation, facecolor='b', alpha=0.7)
+ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
+ax0.fill_between(x_momentum, momentum0, neutral_activation, facecolor='g', alpha=0.7)
+ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
+ax0.fill_between(x_momentum, momentum0, bearish_activation, facecolor='r', alpha=0.7)
+ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
 ax0.set_title('Output membership activity')
 
 # Turn off top/right axes
@@ -95,25 +94,25 @@ for ax in (ax0,):
 plt.tight_layout()
 
 # Aggregate all three output membership functions together
-aggregated = np.fmax(sell_activation,
-                     np.fmax(buy_activation, hold_activation))
+aggregated = np.fmax(bullish_activation,
+                     np.fmax(bearish_activation, neutral_activation))
 
 # Calculate defuzzified result
-stockaction = fuzz.defuzz(x_stockaction, aggregated, 'centroid')
-tip_activation = fuzz.interp_membership(x_stockaction, aggregated, stockaction)  # for plot
+momentum = fuzz.defuzz(x_momentum, aggregated, 'centroid')
+momentum_activation = fuzz.interp_membership(x_momentum, aggregated, momentum)  # for plot
 
 # Visualize this
 fig, ax0 = plt.subplots(figsize=(8, 3))
 
-ax0.plot(x_stockaction, stockaction_sell, 'b', linewidth=0.5, linestyle='--', )
-ax0.plot(x_stockaction, stockaction_hold, 'g', linewidth=0.5, linestyle='--')
-ax0.plot(x_stockaction, stockaction_buy, 'r', linewidth=0.5, linestyle='--')
+ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
+ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
+ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
 
-ax0.fill_between(x_stockaction, stockaction0, aggregated, facecolor='Orange', alpha=0.7)
-ax0.plot([stockaction, stockaction], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
+ax0.fill_between(x_momentum, momentum0, aggregated, facecolor='Orange', alpha=0.7)
+ax0.plot([momentum, momentum], [0, momentum_activation], 'k', linewidth=1.5, alpha=0.9)
 ax0.set_title('Aggregated membership and result (line)')
 
-print(tip_activation)
+print('The crisp value for momentum : ' + str(momentum))
 
 # Turn off top/right axes
 for ax in (ax0,):
