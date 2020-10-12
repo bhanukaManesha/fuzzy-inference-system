03a559e7ede818cca6f52f2d843c0cbdbab9d2c6
bhanukaManesha <bhanukamgamage@gmail.com>
2020-10-12 10:44:00 +0800

    Update main to run multiple experiments ✨

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Generate universe variables
 #   * RSI and MACD is has a range [0, 1]
@@ -14,8 +15,8 @@ rsi_low = fuzz.trapmf(x_rsi, [0, 0, 1, 5])
 rsi_middle = fuzz.trimf(x_rsi, [3, 5, 7])
 rsi_high = fuzz.trapmf(x_rsi, [5, 9, 10, 10])
 
-macd_high = fuzz.trapmf(x_macd, [4, 6, 10, 10])
-macd_low = fuzz.trapmf(x_macd, [0, 0, 4, 6])
+macd_above = fuzz.trapmf(x_macd, [4, 6, 10, 10])
+macd_below = fuzz.trapmf(x_macd, [0, 0, 4, 6])
 
 momentum_bearish = fuzz.trimf(x_momentum, [0, 0, 4])
 momentum_neutral = fuzz.trapmf(x_momentum, [2, 4, 6, 8])
@@ -30,8 +31,8 @@ ax0.plot(x_rsi, rsi_high, 'r', linewidth=1.5, label='High')
 ax0.set_title('RSI')
 ax0.legend()
 
-ax1.plot(x_macd, macd_low, 'b', linewidth=1.5, label='Low')
-ax1.plot(x_macd, macd_high, 'g', linewidth=1.5, label='High')
+ax1.plot(x_macd, macd_below, 'b', linewidth=1.5, label='Below')
+ax1.plot(x_macd, macd_above, 'g', linewidth=1.5, label='Above')
 ax1.set_title('MACD')
 ax1.legend()
 
@@ -49,81 +50,99 @@ for ax in (ax0, ax1, ax2):
     ax.get_yaxis().tick_left()
 
 plt.tight_layout()
+plt.savefig('plots/membership_functions.png')
 
-RSI = 7
-MACD = 5
+results = []
+
+RSI = np.arange(0, 2, 0.5)
+MACD = np.arange(0, 2)
+
+for rsi in RSI:
+    for macd in MACD:
+
+        print('RSI (' + str(rsi) + ') \t MACD (' + str(macd) + ')')
+
+        # We need the activation of our fuzzy membership functions at these values.
+        # This is what fuzz.interp_membership exists for!
+        rsi_level_lo = fuzz.interp_membership(x_rsi, rsi_low, rsi)
+        rsi_level_md = fuzz.interp_membership(x_rsi, rsi_middle, rsi)
+        rsi_level_hi = fuzz.interp_membership(x_rsi, rsi_high, rsi)
+
+        macd_level_below = fuzz.interp_membership(x_macd, macd_below, macd)
+        macd_level_above = fuzz.interp_membership(x_macd, macd_above, macd)
+
+        active_rule2 = np.fmin(macd_level_below, np.fmax(rsi_level_lo, rsi_level_md))
+        active_rule3 = np.fmin(macd_level_above, np.fmax(rsi_level_lo, rsi_level_md))
+
+        bullish_activation = np.fmin(rsi_level_hi, momentum_bullish)
+        neutral_activation = np.fmin(active_rule2, momentum_neutral)
+        bearish_activation = np.fmin(active_rule3, momentum_bearish)
+
+        momentum0 = np.zeros_like(x_momentum)
 
 
-# We need the activation of our fuzzy membership functions at these values.
-# This is what fuzz.interp_membership exists for!
-rsi_level_lo = fuzz.interp_membership(x_rsi, rsi_low, RSI)
-rsi_level_md = fuzz.interp_membership(x_rsi, rsi_middle, RSI)
-rsi_level_hi = fuzz.interp_membership(x_rsi, rsi_high, RSI)
+        # Visualize this
+        fig, ax0 = plt.subplots(figsize=(8, 3))
 
-macd_level_lo = fuzz.interp_membership(x_macd, macd_low, MACD)
-macd_level_hi = fuzz.interp_membership(x_macd, macd_high, MACD)
+        ax0.fill_between(x_momentum, momentum0, bullish_activation, facecolor='b', alpha=0.7)
+        ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
+        ax0.fill_between(x_momentum, momentum0, neutral_activation, facecolor='g', alpha=0.7)
+        ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
+        ax0.fill_between(x_momentum, momentum0, bearish_activation, facecolor='r', alpha=0.7)
+        ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
+        ax0.set_title('Output membership activity')
 
-active_rule2 = np.fmin(macd_level_lo, np.fmax(rsi_level_lo,rsi_level_md))
-active_rule3 = np.fmin(macd_level_hi, np.fmax(rsi_level_lo,rsi_level_md))
+        # Turn off top/right axes
+        for ax in (ax0,):
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.get_xaxis().tick_bottom()
+            ax.get_yaxis().tick_left()
 
-bullish_activation = np.fmin(rsi_level_hi, momentum_bullish)
-neutral_activation = np.fmin(active_rule2, momentum_neutral)
-bearish_activation = np.fmin(active_rule3, momentum_bearish)
+        plt.tight_layout()
 
-momentum0 = np.zeros_like(x_momentum)
+        exp_name = 'RSI (' + str(rsi) + ')_MACD (' + str(macd) + ')'
+        plt.savefig('plots/outputs/'+exp_name+'.png')
+
+        # Aggregate all three output membership functions together
+        aggregated = np.fmax(bullish_activation,
+                             np.fmax(bearish_activation, neutral_activation))
+
+        # Calculate defuzzified result
+        momentum_centroid = fuzz.defuzz(x_momentum, aggregated, 'centroid')
+        momentum_bisector = fuzz.defuzz(x_momentum, aggregated, 'bisector')
+        momentum_activation_centroid = fuzz.interp_membership(x_momentum, aggregated, momentum_centroid)  # for plot
+        momentum_activation_bisector = fuzz.interp_membership(x_momentum, aggregated, momentum_bisector)  # for plot
+
+        # Visualize this
+        fig, ax0 = plt.subplots(figsize=(8, 3))
+
+        ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
+        ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
+        ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
+
+        ax0.fill_between(x_momentum, momentum0, aggregated, facecolor='Orange', alpha=0.7)
+        ax0.plot([momentum_centroid, momentum_centroid], [0, momentum_activation_centroid], 'k', color='blue', linewidth=1.5, alpha=0.9, label='centroid')
+        ax0.plot([momentum_bisector, momentum_bisector], [0, momentum_activation_bisector], 'k', color='red', linewidth=1.5, alpha=0.9, label='bisector')
+        ax0.set_title('Aggregated membership and result (line)')
+        ax0.legend()
+
+        # Turn off top/right axes
+        for ax in (ax0,):
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.get_xaxis().tick_bottom()
+            ax.get_yaxis().tick_left()
+
+        plt.tight_layout()
+        exp_name = 'RSI (' + str(rsi) + ')_MACD (' + str(macd) + ')'
+        plt.savefig('plots/defuzz/'+exp_name+'.png')
+
+        results.append([rsi, macd, momentum_centroid, momentum_bisector])
 
 
-# Visualize this
-fig, ax0 = plt.subplots(figsize=(8, 3))
-
-ax0.fill_between(x_momentum, momentum0, bullish_activation, facecolor='b', alpha=0.7)
-ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
-ax0.fill_between(x_momentum, momentum0, neutral_activation, facecolor='g', alpha=0.7)
-ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
-ax0.fill_between(x_momentum, momentum0, bearish_activation, facecolor='r', alpha=0.7)
-ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
-ax0.set_title('Output membership activity')
-
-# Turn off top/right axes
-for ax in (ax0,):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
-
-# Aggregate all three output membership functions together
-aggregated = np.fmax(bullish_activation,
-                     np.fmax(bearish_activation, neutral_activation))
-
-# Calculate defuzzified result
-momentum = fuzz.defuzz(x_momentum, aggregated, 'centroid')
-momentum_activation = fuzz.interp_membership(x_momentum, aggregated, momentum)  # for plot
-
-# Visualize this
-fig, ax0 = plt.subplots(figsize=(8, 3))
-
-ax0.plot(x_momentum, momentum_bullish, 'b', linewidth=0.5, linestyle='--', )
-ax0.plot(x_momentum, momentum_neutral, 'g', linewidth=0.5, linestyle='--')
-ax0.plot(x_momentum, momentum_bearish, 'r', linewidth=0.5, linestyle='--')
-
-ax0.fill_between(x_momentum, momentum0, aggregated, facecolor='Orange', alpha=0.7)
-ax0.plot([momentum, momentum], [0, momentum_activation], 'k', linewidth=1.5, alpha=0.9)
-ax0.set_title('Aggregated membership and result (line)')
-
-print('The crisp value for momentum : ' + str(momentum))
-
-# Turn off top/right axes
-for ax in (ax0,):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
-
-plt.show()
+a = pd.DataFrame(np.asarray(results), columns=['RSI', 'MACD', 'Centroid', 'Bisector'])
+a.to_csv("results.csv")
 
 
 
